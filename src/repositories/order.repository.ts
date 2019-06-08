fe2d2a9083314a8426d51ea41775b90b85ae9449
Realetive <realetive@yandex.ru>
2019-06-08 12:12:58 +0300

    Add mongo datasource

---
+++ src/repositories/order.repository.ts
@@ -0,0 +1,16 @@
+import {DefaultCrudRepository, juggler} from '@loopback/repository';
+import {Order} from '../models';
+import {MongoDataSource} from '../datasources';
+import {inject} from '@loopback/core';
+
+export class OrderRepository extends DefaultCrudRepository<
+  Order,
+  typeof Order.prototype.id
+> {
+  constructor(
+    // @inject('datasources.mongo') dataSource: MongoDataSource,
+    @inject('datasources.mongo') protected dataSource: juggler.DataSource,
+  ) {
+    super(Order, dataSource);
+  }
+}
