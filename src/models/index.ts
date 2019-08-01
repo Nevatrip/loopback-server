f44b52b84b2c76cc94e20721f1b5ecc4bfd28763
Realetive <realetive@yandex.ru>
2019-08-01 21:04:35 +0300

    Return Nevatravel controller

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './nevatravel.model';
+// export * from './product.model';
+// export * from './cart.model';
+// export * from './cart-product.model';
+// export * from './cart-direction.model';
+// export * from './order.model';
