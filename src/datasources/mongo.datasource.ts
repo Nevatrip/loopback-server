11b137cc2a5352e69ed949dd74f0b557793e9331
Realetive <realetive@yandex.ru>
2019-11-01 08:50:08 +0000

    Replace Mongo to Yandex

--- src/datasources/mongo.datasource.ts
+++ src/datasources/mongo.datasource.ts
@@ -1,10 +1,44 @@
+import {format} from 'util';
+import {readFileSync} from 'fs';
+
 import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
-import * as JSONConfig from './mongo.datasource.json';
+// import * as JSONConfig from './mongo.datasource.json';
 
-const config = { ...JSONConfig };
+// const config = { ...JSONConfig };
 
-config.host = process.env.MONGO_HOST || JSONConfig.host;
+// config.host = process.env.MONGO_HOST || JSONConfig.host;
+
+const mongoUser = process.env.MONGO_USER;
+const mongoPass = process.env.MONGO_PASS;
+const mongoHost = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT
+  ? parseInt(process.env.MONGO_PORT, 10)
+  : 27018;
+const mongoDBName = process.env.MONGO_DB_NAME;
+const mongoReplica = process.env.MONGO_REPLICA;
+const mongoSSLCA = process.env.MONGO_SSLCA;
+
+const config = {
+  name: 'mongo',
+  connector: 'mongodb',
+  url: format(
+    'mongodb://%s:%s@%s/%s?replicaSet=%s&authSource=%s&ssl=true',
+    mongoUser,
+    mongoPass,
+    [`${mongoHost}:${mongoPort}`].join(','),
+    mongoDBName,
+    mongoReplica,
+    mongoDBName,
+  ),
+  sslCA: mongoSSLCA ? readFileSync(mongoSSLCA) : '',
+  host: mongoHost,
+  port: mongoPort,
+  user: mongoUser,
+  password: mongoPass,
+  database: mongoDBName,
+  useNewUrlParser: true,
+};
 
 export class MongoDataSource extends juggler.DataSource {
   static dataSourceName = 'mongo';
