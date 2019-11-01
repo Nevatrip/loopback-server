7cbc7257ac709bfa905748bbe0e1fba109d2e21e
Realetive <realetive@yandex.ru>
2019-11-01 08:53:02 +0000

    Fix timezones

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,8 +1,8 @@
 import {inject} from '@loopback/core';
 import {get, param} from '@loopback/rest';
 import {SanityService} from '../services/sanity.service';
-import {parse} from 'date-fns';
-import {convertToTimeZone} from 'date-fns-timezone';
+import {parse, format} from 'date-fns';
+import {findTimeZone, getUTCOffset} from 'timezone-support';
 import {Product, DirectionProduct, IAction} from '../models';
 
 export class ProductController {
@@ -16,7 +16,7 @@ export class ProductController {
     summary: 'Get Product data by Id',
   })
   async getProductById(@param.path.string('id') id: string): Promise<Product> {
-    const product = (await this.sanityService.getProductById(id))[0];
+    const [product] = await this.sanityService.getProductById(id);
 
     return product;
   }
@@ -26,28 +26,43 @@ export class ProductController {
     summary: 'Get Product data for cart by Id',
   })
   async getProductForCartById(@param.path.string('id') id: string) {
-    const product = (await this.sanityService.getProductForCartById(id))[0];
+    const [product] = await this.sanityService.getProductForCartById(id);
 
     product.directions.forEach(direction => {
-      if (direction._type === 'direction') {
-        let dates: number[] = [];
-        direction.schedule.forEach(event => {
-          const timeZone = event.startTimezone || 'Europe/Moscow';
-          const offsetDate: Date = convertToTimeZone(new Date(), {timeZone});
-          offsetDate.setMinutes(
-            offsetDate.getMinutes() + (direction.buyTimeOffset || 0),
-          );
+      if (direction._type !== 'direction') return;
 
-          event.actions.forEach(action => {
-            const actionDate = convertToTimeZone(action.start, {timeZone});
-            if (actionDate > offsetDate) {
-              dates.push(actionDate.getTime() / 1000);
-            }
-          });
+      const {
+        buyTimeOffset = 0,
+        schedule: [{startTimezone, start}],
+      } = direction;
+      const timeZone = findTimeZone(startTimezone || 'Europe/Moscow');
+      let dates: {[key: string]: Date[]} = {};
+
+      // Смещение часового пояса в минутах для каждого направления
+      direction.timeOffset = getUTCOffset(new Date(start), timeZone).offset;
+
+      direction.schedule.forEach(event => {
+        const offsetDate = new Date();
+        offsetDate.setMinutes(offsetDate.getMinutes() + buyTimeOffset);
+
+        event.actions.forEach(action => {
+          const actionDate = new Date(action.start);
+          if (actionDate > offsetDate) {
+            actionDate.setMinutes(
+              actionDate.getMinutes() - direction.timeOffset,
+            );
+            const dateKey = format(actionDate, 'yyyy-MM-dd');
+            dates[dateKey] = dates[dateKey] || [];
+            dates[dateKey].push(actionDate);
+          }
         });
-        delete direction.schedule;
-        direction.dates = [...new Set(dates)].sort();
-      }
+      });
+      delete direction.schedule;
+      direction.dates = Object.keys(dates)
+        .map(date => new Date(date))
+        .sort((a, b) => {
+          return a < b ? -1 : a > b ? 1 : 0;
+        });
     });
 
     return product;
@@ -62,7 +77,7 @@ export class ProductController {
     @param.path.string('directionId') directionId: string,
     @param.path.string('date') date: string,
   ) {
-    const product = (await this.sanityService.getProductById(id))[0];
+    const [product] = await this.sanityService.getProductById(id);
 
     const directions: {[key: string]: DirectionProduct} = {};
     product.directions.forEach(item => {
@@ -70,32 +85,31 @@ export class ProductController {
     });
 
     if (!directions.hasOwnProperty(directionId)) return;
-    const direction = directions[directionId];
-    if (!direction.schedule || !direction.schedule.length) return;
+    const {schedule, buyTimeOffset = 0} = directions[directionId];
+    if (!schedule || !schedule.length) return;
 
-    let schedule: IAction[] = [];
-    direction.schedule.forEach(event => {
-      const timeZone = event.startTimezone || 'Europe/Moscow';
-      const offsetDate: Date = convertToTimeZone(new Date(), {timeZone});
-      offsetDate.setMinutes(
-        offsetDate.getMinutes() + (direction.buyTimeOffset || 0),
-      );
-      const actualDate = convertToTimeZone(
-        parse(date, 'yyyy-MM-dd', offsetDate),
-        {timeZone},
-      );
+    let scheduleArray: IAction[] = [];
+    schedule.forEach(event => {
+      const offsetDate: Date = new Date();
+      offsetDate.setMinutes(offsetDate.getMinutes() + buyTimeOffset);
+      const actualDate = parse(date, 'yyyy-MM-dd', offsetDate);
+
+      const timeZone = findTimeZone(event.startTimezone || 'Europe/Moscow');
+      const timeOffset = getUTCOffset(new Date(event.start), timeZone).offset;
 
       event.actions.forEach(action => {
-        const actionDate = convertToTimeZone(action.start, {timeZone});
+        const actionDate = new Date(action.start);
+        actionDate.setMinutes(actionDate.getMinutes() - timeOffset);
+
         if (
           actionDate > offsetDate &&
           actionDate.toDateString() === actualDate.toDateString()
         ) {
-          schedule.push(action);
+          scheduleArray.push(action);
         }
       });
     });
 
-    return schedule;
+    return scheduleArray;
   }
 }
